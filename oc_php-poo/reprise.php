c0ada249bab0502ea3e3b47c909d68f597fae369
peterVoisin <peter@voisin-it.fr>
2019-07-29 15:11:09 +0200

    Manipulation de données stockées

---
+++ oc_php-poo/reprise.php
@@ -0,0 +1,93 @@
+<?php
+class Personnage
+{
+  private $_degats; // Les dégâts du personnage.
+  private $_experience; // L'expérience du personnage.
+  private $_force; // La force du personnage (plus elle est grande, plus l'attaque est puissante).
+
+  const FORCE_PETITE = 20;
+  const FORCE_MOYENNE = 50;
+  const FORCE_GRANDE = 80;
+
+  private static $_texteADire = 'Je vais tous vous tuer !';
+
+  public function __construct($forceInitiale)
+  {
+    $this->setForce($forceInitiale);
+  }
+
+  public function frapper(Personnage $persoAFrapper)
+  {
+    $persoAFrapper->_degats += $this->_force;
+  }
+
+  public function gagnerExperience()
+  {
+    // On ajoute 1 à notre attribut $_experience.
+    $this->_experience++;
+  }
+
+  // Mutateur chargé de modifier l'attribut $_force.
+  public function setForce($force)
+  {
+    if (in_array($force, [self::FORCE_PETITE], self::FORCE_MOYENNE, self::[FORCE_GRANDE])) {
+      $this->_force = $force;
+    }
+  }
+
+  // Mutateur chargé de modifier l'attribut $_experience.
+  public function setExperience($experience)
+  {
+    if (!is_int($experience)) // S'il ne s'agit pas d'un nombre entier.
+    {
+      trigger_error('L\'expérience d\'un personnage doit être un nombre entier', E_USER_WARNING);
+      return;
+    }
+
+    if ($experience > 100) // On vérifie bien qu'on ne souhaite pas assigner une valeur supérieure à 100.
+    {
+      trigger_error('L\'expérience d\'un personnage ne peut dépasser 100', E_USER_WARNING);
+      return;
+    }
+
+    $this->_experience = $experience;
+  }
+
+  // Ceci est la méthode degats() : elle se charge de renvoyer le contenu de l'attribut $_degats.
+  public function degats()
+  {
+    return $this->_degats;
+  }
+
+  // Ceci est la méthode force() : elle se charge de renvoyer le contenu de l'attribut $_force.
+  public function force()
+  {
+    return $this->_force;
+  }
+
+  // Ceci est la méthode experience() : elle se charge de renvoyer le contenu de l'attribut $_experience.
+  public function experience()
+  {
+    return $this->_experience;
+  }
+}
+
+$perso1 = new Personnage(Personnage::FORCE_MOYENNE);
+$perso2 = new Personnage(Personnage::FORCE_GRANDE);
+
+/* Ajout du concstructeur
+$perso1->setForce(10);
+$perso1->setExperience(2);
+
+$perso2->setForce(90);
+$perso2->setExperience(58);*/
+
+$perso1->frapper($perso2);
+$perso1->gagnerExperience();
+
+$perso2->frapper($perso1);
+$perso2->gagnerExperience();
+
+echo 'Le personnage 1 a ', $perso1->force(), ' de force, contrairement au personnage 2 qui a ', $perso2->force(), ' de force.<br/>';
+echo 'Le personnage 1 a ', $perso1->experience(), ' d\'expérience, contrairement au personnage 2 qui a ', $perso2->experience(), ' d\'expérience.<br/>';
+echo 'Le personnage 1 a ', $perso1->degats(), ' de dégâts, contrairement au personnage 2 qui a ', $perso2->degats(), ' de dégâts.<br/>';
