9506cf64daa8a04923104e9dfa46b4eaa2cb6e4e
peterVoisin <peter@voisin-it.fr>
2019-08-02 12:00:55 +0200

    Cours MVC chapitre 1

---
+++ oc_php-mvc/model.php
@@ -0,0 +1,16 @@
+<?php
+function getPosts()
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=oc_courses;charset=utf8', 'root', 'root');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+
+    return $req;
+}
