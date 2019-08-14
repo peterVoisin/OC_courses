fb02ef3cd816a97678771afc23d6101bfde83ef9
peterVoisin <peter@voisin-it.fr>
2019-08-14 11:50:50 +0200

    PHP-POO - Retour sur les interfaces

--- oc_php-poo/index.php
+++ oc_php-poo/index.php
@@ -1,38 +1,101 @@
 <?php
-class MonException extends Exception
+class MaClasse implements SeekableIterator, ArrayAccess, Countable
 {
-  public function __construct($message, $code = 0)
+  private $position = 0;
+  private $tableau = ['Premier élément', 'Deuxième élément', 'Troisième élément', 'Quatrième élément', 'Cinquième élément'];
+
+  // Méthode de l'interface SeekableIterator
+
+  public function current()
   {
-    parent::__construct($message, $code);
+    return $this->tableau[$this->position];
   }
 
-  public function __toString()
+  public function key()
   {
-    return $this->message;
+    return $this->position;
+  }
+
+  public function next()
+  {
+    $this->position++;
+  }
+
+  public function rewind()
+  {
+    $this->position = 0;
+  }
+
+  public function seek($position)
+  {
+    $oldPosition = $this->position;
+    $this->position = $position;
+
+    if (!$this->valid()) {
+      trigger_error('La position spécifiée n\'est pas valide', E_USER_WARNING);
+      $this->position = $oldPosition;
+    }
+  }
+
+  public function valid()
+  {
+    return isset($this->tableau[$this->position]);
+  }
+
+  // Méthode de l'interface ArrayAccess
+
+  public function offsetExists($key)
+  {
+    return isset($this->tableau[$key]);
+  }
+
+  public function offsetGet($key)
+  {
+    return $this->tableau[$key];
+  }
+
+  public function offsetSet($key,$value)
+  {
+    $this->tableau[$key] = $value;
+  }
+
+  public function offsetUnset($key)
+  {
+    unset($this->tableau[$key]);
+  }
+
+  // Méthode de l'interface Countable
+
+  public function count()
+  {
+    return count($this->tableau);
   }
 }
 
-function additionner($a, $b)
-{
-  if (!is_numeric($a) || !is_numeric($b)) {
-    // On lance une nouvelle exception grâce à throw et on instancie directement un objet de la classe Exception.
-    throw new MonException('Les deux paramètres doivent être des nombres');
-  }
+$objet = new MaClasse;
 
-  if (func_num_args() > 2) {
-    throw new Exception('Pas plus de deux arguments ne doivent $etre passés à la fonction');
-  }
-
-  return $a + $b;
+foreach ($objet as $key => $value) {
+  echo $key.' => '.$value.'<br/>';
 }
 
-try {
-  echo additionner(12, 3), '<br />';
-  echo additionner(15, 54, 45), '<br />';
-} catch (MonException $e) {
-  echo '[MonException] : '.$e;
-} catch (Exception $e) {
-  echo '[Exception] : '.$e->getMessage();
+echo '<br/>Remise du curseur en troisième position...>br/>';
+$objet->seek(2);
+echo 'Élément courant : '.$objet->current().'<br/>';
+
+echo '<br/>Affichage du troisième élément : '.$objet[2].'<br/>';
+echo 'Modification du troisième élément... ';
+$objet[2] = 'Hello world!';
+echo 'Nouvelle valeur : '.$objet[2].'<br/><br/>';
+
+echo 'Actuellement, mon tableau comporte '.count($objet).' entrées<br/><br/>';
+
+echo 'Destruction du quatrième élément...<br/>';
+unset($objet[3]);
+
+if (isset($objet[3])) {
+  echo '$objet[3] existe toujours... Bizarre...';
+} else {
+  echo 'Tout se passe bien, $objet[3] n\'existe plus !';
 }
 
-echo '<br/>Fin du script';
+echo '<br/><br/>Maintenant, il n\'en comporte plus que '.count($objet).' !';
