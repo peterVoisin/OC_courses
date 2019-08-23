04e6b258044e5f7e8862d3ec21c93fa9a8df173d
peterVoisin <peter@voisin-it.fr>
2019-08-23 09:43:39 +0200

    PHP-POO - La bibliothèque

--- oc_php-poo/tp4/lib/OCFram/HTTPResponse.php
+++ oc_php-poo/tp4/lib/OCFram/HTTPResponse.php
@@ -1,7 +1,7 @@
 <?php
 namespace OCFram;
 
-class HTTPResponse
+class HTTPResponse extends ApplicationComponent
 {
   protected $page;
 
@@ -18,7 +18,12 @@ class HTTPResponse
 
   public function redirect404()
   {
+    $this->page = new Page($this->app);
+    $this->page->setContentFile(__DIR__.'/../../Errors/404.html');
 
+    $this->addHeader('HTTP/1.0 404 Not Found');
+
+    $this->send();
   }
 
   public function send()
